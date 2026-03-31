11f6ea3edb949ace66b69eac97595947a668b8ca
Quiroga Leandro <quirogaleandromanuel@gmail.com>
2026-03-30 22:45:17 -0300

    Implemento diseño responsive

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,21 @@
-import { Component } from '@angular/core';
-import { RouterOutlet, RouterLink } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { RouterOutlet, RouterLink, Router } from '@angular/router';
 import { ChatListComponent } from './components/chat-list/chat-list';
+import { CommonModule } from '@angular/common'; 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, ChatListComponent], 
+  imports: [RouterOutlet, RouterLink, ChatListComponent, CommonModule], 
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class AppComponent {
   title = 'chat-clon-utn';
+  router = inject(Router);
+
+  
+  esChatSeleccionado(): boolean {
+    return this.router.url.includes('/chats/');
+  }
 }
